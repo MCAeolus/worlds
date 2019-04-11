45cb31501036f2aede384f6aaba42ccb42d1bd31
Nathan Smith <mcaeolusch@gmail.com>
2019-04-11 19:56:01 -0400

    small update

--- bluetoothServer/javaserver/src/JavaServer.kt
+++ bluetoothServer/javaserver/src/JavaServer.kt
@@ -1,17 +1,30 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import javax.bluetooth.DiscoveryAgent
+import javax.bluetooth.LocalDevice
 import javax.bluetooth.RemoteDevice
 import javax.bluetooth.UUID
 import javax.microedition.io.Connector
 import javax.microedition.io.StreamConnectionNotifier
 
 
+
+
+
+
+
+val uuid = UUID("1101", true)
+
 fun main(args: Array<String>) {
+
+    LocalDevice.getLocalDevice().discoverable = DiscoveryAgent.GIAC
+
+
+
     runServer()
 }
 
 private fun runServer() {
-    val uuid = UUID("1101", true)
 
     val connectString = "btspp://localhost:$uuid;name=JSON Transfer Server"
     val streamCon = Connector.open(connectString) as StreamConnectionNotifier
@@ -20,7 +33,7 @@ private fun runServer() {
     val connection = streamCon.acceptAndOpen()
 
     val device = RemoteDevice.getRemoteDevice(connection)
-    println("Address ${device.bluetoothAddress}, Name ${device.getFriendlyName(true)}")
+    println("Address ${device.bluetoothAddress}, Name ${device.getFriendlyName(false)}")
 
     val inputStream = connection.openInputStream()
     val reader = BufferedReader(InputStreamReader(inputStream))
